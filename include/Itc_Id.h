8e9eb3ec3288bf8c9bdcfa4ff3612f9ef1a1ad9e
Stanislav Dimov <stanislav.vdimov@gmail.com>
2024-03-27 22:27:16 +0000

    clarify b_IsOwner member usage

--- include/Itc_Id.h
+++ include/Itc_Id.h
@@ -21,7 +21,8 @@ typedef struct ITC_Id_t
     /** A pointer to the parent ID subtree. NULL if root */
     struct ITC_Id_t *pt_Parent;
     /** Determines whether the interval (or subinterval) represented by this
-     * ID is owned by it (i.e. it can be used to inflate events) or not */
+     * ID is owned by it (i.e. it can be used to inflate events) or not.
+     * Parent (i.e. not leaf IDs) should always have this set to `false` */
     bool b_IsOwner;
 } ITC_Id_t;
 
