91d4ffe59bd5107757368755477df4004950bd6e
Stanislav Dimov <stanislav.vdimov@gmail.com>
2024-03-29 20:14:52 +0000

    WIP: Add meson build system

---
+++ libitc/test/ITC_Id_Test.c
@@ -0,0 +1,17 @@
+#include "unity.h"
+
+#include "ITC_Id_Test.h"
+#include "ITC_Id.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void ITC_Id_Test_something(void)
+{
+    TEST_ASSERT_FALSE(0);
+}
