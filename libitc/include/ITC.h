2c9518ef48601616e83eec91231ca30042271670
Stanislav Dimov <stanislav.vdimov@gmail.com>
2024-04-18 19:53:47 +0100

    Create an umbrella header

---
+++ libitc/include/ITC.h
@@ -0,0 +1,17 @@
+/**
+ * @file ITC.h
+ * @author Stan
+ * @brief The Interval Tree Clocks (ITC) library
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+#ifndef ITC_H_
+#define ITC_H_
+
+#include "ITC_Event.h"
+#include "ITC_Id.h"
+#include "ITC_Stamp.h"
+#include "ITC_Status.h"
+
+#endif /* ITC_H_ */
