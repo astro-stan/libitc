6d57bf86dcdcdc2beb04f8fd96c1f27c84bb9f81
Stanislav Dimov <stanislav.vdimov@gmail.com>
2024-04-23 21:25:25 +0100

    Overhaul build system; add mocking capability

---
+++ libitc/test/mocked/ITC_Id_Test.c
@@ -0,0 +1,14 @@
+#include "ITC_Id.h"
+#include "ITC_Test_package.h"
+
+// #include "MockITC_Port.h"
+
+/******************************************************************************
+ *  Public functions
+ ******************************************************************************/
+
+/* Init test */
+void setUp(void) {}
+
+/* Fini test */
+void tearDown(void) {}
